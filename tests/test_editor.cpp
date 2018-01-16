c498b72d39b46473ea2fcb3ff1df0d81475d4185
Elias Farhan <elias.farhan@gmail.com>
2018-01-16 15:55:04 +0100

    Adding editor test

---
+++ tests/test_editor.cpp
@@ -0,0 +1,15 @@
+#include <engine/engine.h>
+#include <engine/scene.h>
+
+int main()
+{
+	sfge::Engine::GetInstance()->Init(false, true);
+	auto sceneManager = std::dynamic_pointer_cast<sfge::SceneManager>(
+		sfge::Engine::GetInstance()->GetModule(sfge::EngineModule::SCENE_MANAGER));
+	sceneManager->SetCurrentScene( sceneManager->LoadScene("data/scenes/test.scene"));
+	sfge::Engine::GetInstance()->Start();
+#ifdef WIN32
+	system("pause");
+#endif
+	return EXIT_SUCCESS;
+}
